579f2b5e5ac993ae735c572eac7c70fd89a79dfd
vagrant <vagrant@myvm19>
2021-08-07 15:10:09 +0000

    First Commit

---
+++ swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d", &a, &b);
+
+    //swapping values of a and b without using third variable
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    printf("a = %d, b = %d", a, b);
+    return 0;
+}
