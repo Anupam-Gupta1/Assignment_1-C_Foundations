b8ad160c669a517be047be1d92f955764b8a8abc
vagrant <vagrant@myvm19>
2021-08-07 16:29:11 +0000

    First Commit

--- pyramid.c
+++ pyramid.c
@@ -3,17 +3,23 @@ int main()
 {
     int i,j,k,n;
     scanf("%d", &n);
-
+    
+    //loop for number of rows of pyramid
     for(i = 1; i <= n; i++)
     {
+	//loop to input spaces before #
         for(j = 1; j <= n-i; j++)
         {
             printf(" ");
         }
+
+	//loop to input # till i
         for(k = 0; k < i; k++)
         {
             printf("# ");
         }
+
+	//to move to next row
         printf("\n");
     }
 
